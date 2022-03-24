d6e0074677e935fbb38310ac1c43305804ab0855
Arvid Teichtmann <arvid.teichtmann@gmx.de>
2022-03-24 12:33:41 +0100

    fixed key names

--- app.py
+++ app.py
@@ -5,35 +5,25 @@ import pydeck as pdk
 import plotly.express as px
 
 
-DATA_URL = "https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv"
+# DATA_URL = "https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv"
 
-st.title("Motor Vehicle Collisions in New York City")
+st.title("Motor Vehicle Collisions in NYC")
 st.markdown("This application is a Streamlit dashboard that can be used to analyze crash data from New York City.")
-
-# @st.cache(persist=True)
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
-#     data.dropna(subset=['LATITUDE','LONGITUDE'], inplace=True)
-#     lowercase = lambda x: str(x).lower()
-#     data.rename(lowercase,axis='columns',inplace=True)
-#     data.rename(columns={'crash_date_crash_time': 'date/time'},inplace=True)
-#     return data
-
-# data = load_data(100000)
 
 @st.cache(persist=True)
 def load_data():
     data = pd.read_parquet('crashes.parquet', engine='pyarrow')
+    # drop rows with no lat/long values:
     data.dropna(subset=['latitude', 'longitude'], inplace=True)
     return data
-
 
 data = load_data()
 original_data = data
 
 st.header("Where are the most people injured in NYC")
-injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
+max_injured_people = data['number_of_persons_injured'].max()
+injured_people = st.slider("Number of persons injured in vehicle collisions", 0, max_injured_people)
+st.map(data.query("number_of_persons_injured >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
 st.header("How many collisions occur during a given time of day?")
 hour = st.slider("Hour to look at", 0, 23)
@@ -80,14 +70,14 @@ st.header("Top 5 dangerous streets by affected type")
 select = st.selectbox('Affected type of people', ['Pedestrians', 'Cyclists', 'Motorists'])
 
 if select == 'Pedestrians':
-    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(
-        by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
+    st.write(original_data.query("number_of_pedestrians_injured >= 1")[["on_street_name", "number_of_pedestrians_injured"]].sort_values(
+        by=['number_of_pedestrians_injured'], ascending=False).dropna(how='any')[:5])
 elif select == 'Cyclist':
-    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(
-        by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
+    st.write(original_data.query("number_of_cyclist_injured >= 1")[["on_street_name", "number_of_cyclist_injured"]].sort_values(
+        by=['number_of_cyclist_injured'], ascending=False).dropna(how='any')[:5])
 else:
-    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(
-        by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
+    st.write(original_data.query("number_of_motorist_injured >= 1")[["on_street_name", "number_of_motorist_injured"]].sort_values(
+        by=['number_of_motorist_injured'], ascending=False).dropna(how='any')[:5])
 
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
